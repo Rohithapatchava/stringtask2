20a0f0ec2e3e0a6c64ea7996e5dd2d5c0ca85d91
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:31:07 +0530

    Initial commit

---
+++ stringtask2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringtask2
+{
+    class Program
+    {
+        static void fullname(string firstname,string lastname)
+        {
+            string s=string.Concat(firstname," ", lastname);
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            fullname("rohitha", "sarah");
+        }
+    }
+}
